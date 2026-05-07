64c327a61b7fb227949abdef2efb51f1acea6232
Tom Watson <smileym4n@gmail.com>
2026-05-07 15:21:17 +0100

    Fix CI secrets file creation

---
+++ include/secrets.example.h
@@ -0,0 +1,11 @@
+#pragma once
+
+/*
+  Copy this file to include/secrets.h and set your local credentials.
+
+  Do not commit include/secrets.h. It should contain private Wi-Fi details for
+  your own build machine only.
+*/
+
+#define SNAP_WIFI_SSID_SECRET "zzzz"
+#define SNAP_WIFI_PASSWORD_SECRET "zzzz"
